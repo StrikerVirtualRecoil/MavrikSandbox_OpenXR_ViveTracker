d709dfea2a4a5ebdd5b79d9e2130b60845419e39
StrikerNorm <106762990+StrikerNorm@users.noreply.github.com>
2022-12-05 14:17:26 -0600

    Updated to match assets in SDK
    Separate scripts for each blaster mode. Assets match SDK. Gravity mode updated to include hologram on selected object. Object shoot forward on trigger up.

--- Assets/Sandbox/Custom Scripts/SceneTeleport.cs
+++ Assets/Sandbox/Custom Scripts/SceneTeleport.cs
@@ -5,28 +5,12 @@ using UnityEngine.SceneManagement;
 
 public class SceneTeleport : MonoBehaviour
 {
-   
-    // Start is called before the first frame update
-    void Start()
+  
+    private void OnCollisionEnter(Collision collision)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        if(other.gameObject.tag == "Home")
-        {
-            SceneManager.LoadScene("StrikerDomeBlasterverse");
-        }
-        else if (other.gameObject.tag == "UFO")
-        {
-            SceneManager.LoadScene("UFHoedown");
+        if (collision.gameObject.CompareTag("Bullet"))
+        { 
+            SceneManager.LoadScene("StrikerVR_Mavrik_Sandbox");
         }
     }
 }
--- Assets/Sandbox/Custom Scripts/UIHandler.cs
+++ Assets/Sandbox/Custom Scripts/UIHandler.cs
@@ -6,53 +6,63 @@ using UnityEngine.UI;
 public class UIHandler : MonoBehaviour
 {
 
-    public Sprite first, fullAuto, ballzooka, gravity, crossbeaux, plasma;
-    public Blaster blaster;
-    private Image image;
-    public Sprite sprite;
+    //public Sprite first, fullAuto, ballzooka, gravity, crossbeaux, plasma;
+    //public Blaster blaster;
+    public ModeHandler modes;
+    //private Image image;
+   // public Sprite sprite;
+    private Animator anim;
+
     // Start is called before the first frame update
     void Start()
     {
-        first = Resources.Load<Sprite>("UI_Sprites/StrikerVR_Sandbox_UI_First");
+        /*first = Resources.Load<Sprite>("UI_Sprites/StrikerVR_Sandbox_UI_First");
         fullAuto = Resources.Load<Sprite>("UI_Sprites/StrikerVR_Sandbox_UI_FullAuto");
         ballzooka = Resources.Load<Sprite>("UI_Sprites/StrikerVR_Sandbox_UI_Ballzooka");
         gravity = Resources.Load<Sprite>("UI_Sprites/StrikerVR_Sandbox_UI_Gravity");
-        crossbeaux = Resources.Load<Sprite>("UI_Sprites/StrikerVR_Sandbox_UI_Crossbeaux");
+        crossbeaux = Resources.Load<Sprite>("UI_Sprites/StrikerVR_Sandbox_UI_Crossbeaux_2");
         plasma = Resources.Load<Sprite>("UI_Sprites/StrikerVR_Sandbox_UI_Plasma");
 
-        image = gameObject.GetComponent<Image>();
+        image = gameObject.GetComponent<Image>();*/
 
-        
+        anim = gameObject.GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        image.sprite = sprite; 
+        //image.sprite = sprite; 
 
-        if (blaster.fireMode == 0)
+        if (modes.fireMode == 0)
         {
-            sprite = first;
+            //sprite = first;
+            anim.SetInteger("Mode", 0); 
         }
-        else if (blaster.fireMode == 1)
+        else if (modes.fireMode == 1)
         {
-            sprite = fullAuto;
+            //sprite = fullAuto;
+            anim.SetInteger("Mode", 1);
         }
-        else if (blaster.fireMode == 2)
+        else if (modes.fireMode == 2)
         {
-            sprite = ballzooka;
+            // sprite = ballzooka;
+            anim.SetInteger("Mode", 2);
         }
-        else if (blaster.fireMode == 3)
+        else if (modes.fireMode == 3)
         {
-            sprite = gravity;
+
+            //sprite = crossbeaux;
+            anim.SetInteger("Mode", 3);
         }
-        else if (blaster.fireMode == 4)
+        else if (modes.fireMode == 4)
         {
-            sprite = crossbeaux;
+            //sprite = gravity;
+            anim.SetInteger("Mode", 4);
         }
-        else if (blaster.fireMode == 5)
+        else if (modes.fireMode == 5)
         {
-            sprite = plasma;
+            //sprite = plasma;
+            anim.SetInteger("Mode", 5);
         }
     }
 }
